f4f333a4f19afce69ec4e99fea91146c4c12ea6c
Abil-Shrestha <abilshr1@gmail.com>
2024-03-15 23:16:08 -0400

    Set State

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -1,32 +1,65 @@
 import 'package:flutter/material.dart';
 
-class CoffeePrefs extends StatelessWidget {
+class CoffeePrefs extends StatefulWidget {
   const CoffeePrefs({super.key});
+
+  @override
+  State<CoffeePrefs> createState() => _CoffeePrefsState();
+}
+
+class _CoffeePrefsState extends State<CoffeePrefs> {
+  int strength = 1;
+  int sugars = 1;
+  void increaseStrength() {
+    setState(() {
+      strength = strength < 5 ? strength + 1 : 1;
+    });
+  }
+
+  void increaseSugars() {
+  setState((){
+    sugars = sugars < 5 ? sugars + 1 : 0;
+  });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       Row(children: [
         const Text('Strength: '),
-        const Text('3'),
+        Text('$strength'),
         Image.asset(
           'assets/img/coffee_bean.png',
           width: 25,
           color: Colors.brown[100],
           colorBlendMode: BlendMode.multiply,
         ),
-        const SizedBox(width: 50)
+        const Expanded(child: SizedBox()),
+        FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.brown,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: increaseStrength,
+            child: const Text('+')),
       ]),
       Row(children: [
         const Text('Sugars: '),
-        const Text('3'),
+        Text('$sugars'),
         Image.asset(
           'assets/img/sugar_cube.png',
           width: 25,
           color: Colors.brown[100],
           colorBlendMode: BlendMode.multiply,
         ),
-        const SizedBox(width: 50)
+        const Expanded(child: SizedBox()),
+        FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.brown,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: increaseSugars,
+            child: const Text('+')),
       ])
     ]);
   }
