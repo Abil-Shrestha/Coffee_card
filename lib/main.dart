662c6ae410a558024e7ad6de55d144e4a63346f7
Abil-Shrestha <abilshr1@gmail.com>
2024-03-15 22:25:22 -0400

    Import/export

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,25 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(home: Home()));
 }
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: const Text('Sandbox'), backgroundColor: Colors.grey),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(width: 100, color: Colors.red, child: const Text('one')),
+            Container(
+                width: 200, color: Colors.yellow, child: const Text('two')),
+            Container(
+                width: 300, color: Colors.green, child: const Text('three')),
+          ]),
+    );
+  }
+}
